128c23cacf6c6dcaa75387857c54bed73f0b4a26
JY Lee <jylee.rahman@gmail.com>
2026-03-21 18:21:22 -0400

    Add font

--- foster-app/vite.config.ts
+++ foster-app/vite.config.ts
@@ -8,12 +8,7 @@ export default defineConfig({
 		react(),
 		VitePWA({
 			registerType: "autoUpdate",
-			includeAssets: [
-				"favicon.ico",
-				"vite.svg",
-				"icon-192.png",
-				"icon-512.png",
-			],
+			includeAssets: ["favicon.ico", "favicon.png", "vite.svg"],
 			// Manifest is read from public/manifest.json automatically
 			workbox: {
 				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
